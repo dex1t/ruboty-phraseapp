082acf4399abe0fd13a23b88125383c9f5620dd9
dex1t <dex1t@degoo.org>
2016-12-01 01:41:34 +0900

    Initial commit

---
+++ lib/ruboty/phraseapp.rb
@@ -0,0 +1,7 @@
+require "ruboty/phraseapp/version"
+
+module Ruboty
+  module Phraseapp
+    # Your code goes here...
+  end
+end
---
+++ lib/ruboty/phraseapp/version.rb
@@ -0,0 +1,5 @@
+module Ruboty
+  module Phraseapp
+    VERSION = "0.1.0"
+  end
+end
